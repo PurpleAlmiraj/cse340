7a90464ed069888357c41f4517d0b6f8b6d32024
Ryan Jones <ryanbluej@gmail.com>
2024-06-08 12:25:00 -0700

    changes

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -6,6 +6,4 @@ router.get("/type/:classificationId", invController.buildByClassificationId);
 
 router.get("/detail/:inventoryId", invController.buildByInventoryId);
 
-router.get('/trigger-error', errorController.triggerError);
-
 module.exports = router;
